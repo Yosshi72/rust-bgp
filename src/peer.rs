a7904dab448b328ad63b3c888e2e2ff49859c0aa
Yosshi72 <faily.nappy.yshk@gmail.com>
2023-07-17 00:51:10 +0900

    update main.rs

--- src/peer.rs
+++ src/peer.rs
@@ -49,16 +49,3 @@ impl Peer {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[tokio::test]
-    async fn peer_can_transition_to_connect_state() {
-        let config: Config = "64512 127.0.0.1 65413 172.0.0.2 active".parse().unwrap();
-        let mut peer = Peer::new(config);
-        // peer.start();
-        peer.next().await;
-        assert_eq!(peer.state, State::Connect);
-    }
-}
